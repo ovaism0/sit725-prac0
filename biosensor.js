a5a673605ca9a637f66bad48c22cd4bce50b60cb
ovaism0 <68365824+ovaism0@users.noreply.github.com>
2020-07-25 04:05:06 +1000

    Update biosensor.js
    cover extra spaces.

--- biosensor.js
+++ biosensor.js
@@ -4,30 +4,21 @@ class Biosensor {
         this.owner=name; 
         this.heartRate=0;
         this.live()
-
     }
-        
         generateRandomArbitrary(min, max){
         return parseInt(Math.random() * (max - min) + min);
-    }
-
-       
+    }  
        generateHeartBeat(){
            let HeartBeat=this.generateRandomArbitrary(60,70);
            this.heartRate=HeartBeat;
            console.log('[Owner]:'+this.owner+' ---- [HR]:'+this.heartRate)
-
         }
-        
-        
+           
         // living function simply starts an endless interval to generate a beat
         live (){
             setInterval(() => {this.generateHeartBeat()},1000)
         }
-
        }
-
-
 let james=new Biosensor('james')
 let Sarah=new Biosensor('Sarah')
 let Anthony=new Biosensor('Anthony')
